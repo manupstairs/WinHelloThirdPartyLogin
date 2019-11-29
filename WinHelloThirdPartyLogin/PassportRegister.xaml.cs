35056ca13151a0ba14df6208a1f946a4ae262744
xingzheng <xingzheng@outlook.com>
2019-11-29 11:17:09 +0800

    Add project files.

---
+++ WinHelloThirdPartyLogin/PassportRegister.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WinHelloThirdPartyLogin.Models;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WinHelloThirdPartyLogin
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PassportRegister : Page
+    {
+        private Account _account;
+        public PassportRegister()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void RegisterButton_Click_Async(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(UsernameTextBox.Text))
+            {
+                //Register a new account
+                _account = AccountHelper.AddAccount(UsernameTextBox.Text,PasswordTextBox.Password);
+                //Register new account with Microsoft Passport
+                await MicrosoftPassportHelper.CreatePassportKeyAsync(_account.Username);
+                //Navigate to the Welcome Screen. 
+                Frame.Navigate(typeof(Welcome), _account);
+            }
+        }
+    }
+}
---
+++ WinHelloThirdPartyLogin/UserList.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WinHelloThirdPartyLogin.Models;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WinHelloThirdPartyLogin
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class UserList : Page
+    {
+        public UserList()
+        {
+            this.InitializeComponent();
+            Loaded += UserList_Loaded;
+        }
+
+        private async void UserList_Loaded(object sender, RoutedEventArgs e)
+        {
+            await AccountHelper.LoadAccountListAsync();
+            if (AccountHelper.AccountList.Count == 0)
+            {
+                //If there are no accounts navigate to the LoginPage
+                Frame.Navigate(typeof(PassportRegister));
+            }
+            UserListView.ItemsSource = AccountHelper.AccountList;
+            UserListView.SelectionChanged += UserSelectionChanged;
+        }
+
+        private void UserSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (((ListView)sender).SelectedValue != null)
+            {
+                Account account = (Account)((ListView)sender).SelectedValue;
+                if (account != null)
+                {
+                    Frame.Navigate(typeof(Welcome), account);
+                }
+                else
+                { 
+                    Frame.Navigate(typeof(PassportRegister));
+                }
+            }
+        }
+
+        private void AddUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(PassportRegister));
+        }
+    }
+}
